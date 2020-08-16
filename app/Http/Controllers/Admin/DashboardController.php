54a8a43cdf29d7da972f418ddec371dffd31ff54
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-08-16 19:21:29 +1000

    Added initial admin controller and views

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+}
