c703a8e71bf764f0c286fcd8d8b5ae57dd0c116f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-21 21:53:13 +1100

    Added create, update, delete test cases

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,20 +2,60 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_create_user_successfully()
+    {
+        $user = factory(User::class)->create();
+
+        $jsonResponse = $this->actingAs($user)
+            ->json('post', '/admin/users', [
+                'email' => 'demo@demo.com',
+                'name' => 'DEMO_NAME',
+            ]);
+
+        $jsonResponse
+            ->assertStatus(201)
+            ->assertJson([
+            'data' => [
+                'name' => 'DEMO_NAME',
+            ]
+        ]);
+    }
+
+    public function test_update_user_successfully()
+    {
+        $user = factory(User::class)->create();
+
+        $jsonResponse = $this->actingAs($user)
+            ->json('put', "/admin/users/$user->id", [
+                'email' => 'updated@demo.com',
+                'name' => 'UPDATED_NAME'
+            ]);
+
+        $jsonResponse
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'email' => 'updated@demo.com',
+                    'name' => 'UPDATED_NAME',
+                ]
+            ]);
+    }
+
+    public function test_delete_user_successfully()
+    {
+        $user = factory(User::class)->create();
+
+        $jsonResponse = $this->actingAs($user)
+            ->json('delete', "/admin/users/$user->id");
+
+        $jsonResponse->assertStatus(204);
     }
 }
