c53a6eb0a4a5ea3af308691404c7216661e86da6
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-21 23:03:22 +1100

    Added roles to user resource

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -19,6 +19,7 @@ class User extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'roles' => $this->roles,
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];
