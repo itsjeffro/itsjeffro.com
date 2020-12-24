b88e30057869e88a609c50fc27ac55f5f65b918b
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-24 16:03:16 +1100

    Renamed component

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from "./Navbar/Navbar";
 
-function Example() {
+function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
+        <>
+            <Navbar
+                title={"ItsJeffro.com"}
+            />
+            <div className="container">
+                <div className="content">
+                    <div className="flex-center">
+                        No posts at the moment.
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
-export default Example;
+export default App;
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<Example />, document.getElementById('app'));
+    ReactDOM.render(<App />, document.getElementById('app'));
 }
