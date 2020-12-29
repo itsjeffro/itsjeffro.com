02f24ed0caab02f3cc11a05c8565ad05794a9cd8
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-29 17:05:58 +1100

    Added user relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
