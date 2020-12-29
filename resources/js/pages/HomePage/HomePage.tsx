b91dc6777bda2a3241b3360ee036b817b7945aa7
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-29 23:24:17 +1100

    Added class and export

---
+++ resources/js/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class HomePage extends React.Component<any, any> {
+  render() {
+    return (
+      <>
+        Home page
+      </>
+    )
+  }
+}
+
+export default HomePage;
