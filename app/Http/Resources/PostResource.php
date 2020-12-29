e50ca35a078aead6f6506b6d760d51b71bdbb58d
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-29 17:05:39 +1100

    Updated with mapped fields

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,16 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'author' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
     }
 }
