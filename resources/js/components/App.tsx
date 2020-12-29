e466ec7b1d07c9272856c241e8814ed70c489465
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-29 17:07:00 +1100

    Load posts

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from "./Navbar/Navbar";
+import PostsApi from "../services/api/PostsApi";
+import Post from "../pages/HomePage/components/Post";
 
-function App() {
+class App extends React.Component<any, any> {
+  state = {
+    isLoading: true,
+    posts: {
+      data: [],
+    },
+  };
+
+  componentDidMount(): void {
+    const postsApi = new PostsApi();
+    
+    const posts = postsApi.getAll()
+      .then((response) => {
+        this.setState({
+          posts: response.data,
+          isLoading: false
+        });
+      }, (error) => {
+        //
+      });
+  }
+  
+  render() {
+    const { posts, isLoading } = this.state;
+    
     return (
-        <>
-            <Navbar
-                title={"ItsJeffro.com"}
-            />
-            <div className="container">
-                <div className="content">
-                    <div className="flex-center">
-                        No posts at the moment.
-                    </div>
-                </div>
-            </div>
-        </>
+      <>
+        <Navbar
+          title={ "ItsJeffro.com" }
+        />
+      
+        <div className="container">
+          <div className="content">
+            { isLoading ? <div className="flex-center">Loading posts ...</div> : ''}
+            
+            { !isLoading && posts.data.length === 0
+              ? <div className="flex-center">No posts at the moment.</div>
+              : '' }
+            
+            { !isLoading ? posts.data.map((post) => <Post key={ post.id } post={ post }/>) : '' }
+          </div>
+        </div>
+      </>
     );
+  }
 }
 
 export default App;
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+  ReactDOM.render(<App />, document.getElementById('app'));
 }
