c47e52d0ec0d3efcfc111c7c2c43ce811a756a7f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-30 12:57:07 +1100

    Added children, removed hardcoded links

--- resources/js/components/Navbar/Navbar.tsx
+++ resources/js/components/Navbar/Navbar.tsx
@@ -2,31 +2,19 @@ import * as React from 'react';
 import NavbarToggle from "./NavbarToggle";
 
 const Navbar = (props) => {
-    const {
-        title,
-        items,
-    } = props;
-
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">{ title }</a>
-
-                <NavbarToggle />
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://github.com/itsjeffro" target="_blank">Github</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  const {
+    title,
+  } = props;
+  
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <a className="navbar-brand" href="#">{ title }</a>
+      
+      <NavbarToggle />
+      
+      { props.children }
+    </nav>
+  );
 };
 
 export default Navbar;
