f22734d9dd953a7004d35c097ff80656ab6786f5
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-30 21:55:40 +1100

    Added laravel sanctum and configurations

--- app/User.php
+++ app/User.php
@@ -6,11 +6,12 @@ use App\Models\Role;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasRoles;
+    use HasApiTokens, Notifiable, HasRoles;
 
     /** @var int */
     const DEFAULT_PER_PAGE = 30;
