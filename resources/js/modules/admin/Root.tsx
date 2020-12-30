97afff7b1992fe52abbea8b1f1a4687e619b583b
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-30 12:57:56 +1100

    User react router

--- resources/js/modules/admin/Root.tsx
+++ resources/js/modules/admin/Root.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import Navbar from "../../components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+/** Pages **/
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import PostsPage from "./pages/PostsPage/PostsPage";
+import UsersPage from "./pages/UsersPage/UsersPage";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
 
 class Root extends React.Component<any, any> {
   render() {
     return (
-      <>
-        <Navbar title="Admin Panel" />
-      </>
+      <Router basename="admin">
+        <>
+          <Switch>
+            <Route exact path="/" component={ DashboardPage } />
+            <Route path="/posts" component={ PostsPage } />
+            <Route path="/users" component={ UsersPage } />
+            <Route path="/settings" component={ SettingsPage } />
+          </Switch>
+        </>
+      </Router>
     );
   }
 }
