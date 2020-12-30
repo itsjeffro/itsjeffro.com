84d2371a23351a9eb27a26fb8bfe8751b57d4028
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-30 14:57:09 +1100

    Added format method

--- resources/js/services/DateTime.ts
+++ resources/js/services/DateTime.ts
@@ -51,4 +51,54 @@ export default class DateTime {
 
     return hours >= 12 ? 'pm' : 'am';
   }
+  
+  public format(dateFormat: string) {
+    let characters = dateFormat.split('');
+    
+    return characters.map((character) => {
+      // Return day, eg, 1
+      if (character === 'd') {
+        return this.getDay();
+      }
+      
+      // Return full month name, eg. January
+      if (character === 'F') {
+        return this.getFullMonth();
+      }
+      
+      // Return full year, eg. 2020
+      if (character === 'Y') {
+        return this.getFullYear();
+      }
+      
+      // Return meridiem, eg. am or pm
+      if (character === 'a') {
+        return this.getMeridiem();
+      }
+      
+      // Return meridiem, eg. AM or PM
+      if (character === 'A') {
+        return this.getMeridiem().toUpperCase();
+      }
+      
+      // Return 12 hour format, eg. 1
+      if (character === 'h') {
+        let hours = this.getHours() % 12;
+        return hours ? hours : 12;
+      }
+      
+      // Return minutes, eg. 59
+      if (character === 'i') {
+        return this.getMinutes();
+      }
+      
+      // Return seconds, eg. 59
+      if (character === 's') {
+        return this.getSeconds();
+      }
+      
+      // Return any other character that has no mapping.
+      return character;
+    }).join('');
+  }
 }
