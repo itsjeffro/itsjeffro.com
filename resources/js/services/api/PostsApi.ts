283066eec84ee1f218bd777157d7d77574c61756
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-12-30 17:09:37 +1100

    Added update, create and delete methods

--- resources/js/services/api/PostsApi.ts
+++ resources/js/services/api/PostsApi.ts
@@ -6,4 +6,20 @@ export default class PostsApi extends BaseApi {
     
     return this.getRequest(`/api/posts?page=${ page }`);
   }
+  
+  public getOne(postId: string) {
+    return this.getRequest(`/api/posts/${ postId }`);
+  }
+  
+  public updateOne(postId: string, data: any) {
+    return this.putRequest(`/api/posts/${ postId }`, data);
+  }
+  
+  public createOne(postId: string, data: any) {
+    return this.postRequest(`/api/posts`, data);
+  }
+  
+  public deleteOne(postId: string) {
+    return this.deleteRequest(`/api/posts/${ postId }`);
+  }
 }
