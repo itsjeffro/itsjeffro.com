fe15c9417fbb5cf963281b5aa74dbf1570c63ea9
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-01 19:30:16 +1100

    Include slug in post resource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -17,6 +17,7 @@ class PostResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'slug' => $this->slug,
             'content' => $this->content,
             'author' => [
                 'id' => $this->user->id,
