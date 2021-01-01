a3c819c68a4168861215743fa9bd8ce7f1e9f3e5
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-01 19:31:55 +1100

    Added property comments to help with autocomplete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,25 @@ use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Post
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $title
+ * @property string $slug
+ * @property string $content
+ * @property \DateTime $published_at
+ */
 class Post extends Model
 {
     use SoftDeletes;
 
+    /** @var int */
+    const DRAFT_STATUS = 1;
+
+    /**
+     * Post belongs to user.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
