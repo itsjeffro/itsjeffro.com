baa62c93fb8fdad731d4d3599e4f612882c4fd19
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-01 19:28:31 +1100

    Added slug

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,12 +4,14 @@
 
 use App\Models\Post;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Post::class, function (Faker $faker) {
     $sentence = $faker->sentence(rand(2,5));
 
     return [
         'title' => substr($sentence, 0, strlen($sentence) - 1),
+        'slug' => Str::slug(substr($sentence, 0, strlen($sentence) - 1)),
         'content' => $faker->paragraph(rand(5, 30)),
     ];
 });
