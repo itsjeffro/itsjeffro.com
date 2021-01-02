3dfaf08ce4ef234895850981fd8f440516ef92c2
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-02 16:54:38 +1100

    Added update and get methods

--- resources/js/services/api/UsersApi.ts
+++ resources/js/services/api/UsersApi.ts
@@ -4,4 +4,12 @@ export default class UsersApi extends BaseApi {
   public getAll() {
     return this.getRequest('/api/users');
   }
+  
+  public getOne(userId: string) {
+    return this.getRequest(`/api/users/${ userId }`);
+  }
+  
+  public updateOne(userId: string, data: any) {
+    return this.putRequest(`/api/users/${ userId }`, data);
+  }
 }
